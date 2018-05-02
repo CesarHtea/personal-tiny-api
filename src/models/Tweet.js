0b617665bed8f13c13db9fe0cc945e447cac3f1a
CesarHtea <chernandez_83@yahoo.com.mx>
2018-05-02 04:45:05 -0500

    inicia refactorizacion

--- src/models/Tweet.js
+++
@@ -1,9 +0,0 @@
-const { Model } = require('objection')
-
-class Tweet extends Model {
-  static get tableName() {
-  	return 'tweets'
-  }
-}
-
-module.exports = Tweet
--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,79 +1,16 @@
 const Router = require('express').Router
-const Tweet = require('../models/Tweet')
-const Lists = require('../models/List')
-
 const apiRouter = Router()
 
-function allTweets(req, res) {
-  Tweet
-    .query()
-    .then(function(data) {
-      res.json(data)
-    })
-}
-
-function getSingleTweet(req, res) {
-  const id = parseInt(req.params.tweetId)
-  Tweet
-    .query()
-    .findById(id)
-    .then(function(tweet) {
-      res.json(tweet).status(200)
-    })
-}
-
-function createNewTweet(req, res) {
-  Tweet
-    .query()
-    .insert(req.body)
-    .then(function(newTweet) {
-      res.json(newTweet).status(200)
-    })
-}
-
-function updateTweet(req, res) {
-  const tweetId = parseInt(req.params.tweetId)
-  const newData = req.body
-
-  Tweet
-    .query()
-    .updateAndFetchById(tweetId, newData)
-    .then(function(tweetUpdated) {
-      res.json(tweetUpdated).status(200)
-    })
-}
-
-function deleteTweet(req, res) {
-  const tweetId = parseInt(req.params.tweetId)
-
-  Tweet
-    .query()
-    .deleteById(tweetId)
-    .then(function(rowsDeleted) {
-      res.json({
-        tweetsDeleted: rowsDeleted
-      }).status(200)
-    })
-}
-
-function allLists(req, res) {
-  Lists
-    .query()
-    .eager('tweets')
-    .then(function(data) {
-      res.json(data)
-    })
-}
 
 apiRouter
 
-  .get('/tweets', allTweets)
-  .get('/tweets/:tweetId', getSingleTweet)
-  .post('/tweets', createNewTweet)
-  .put('/tweets/:tweetId', updateTweet)
-  .delete('/tweets/:tweetId', deleteTweet)
+  .get('/...', cbAll)
+  .get('/.../:...', cbGetSingle)
+  .post('/...', cbCreateNew)
+  .put('/.../:...', cbUpdate)
+  .delete('/.../:...', cbDelete)
 
-  .get('/lists', allLists)
+
 
 
 module.exports = apiRouter
