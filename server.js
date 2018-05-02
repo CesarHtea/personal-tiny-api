0baa42b87bd847689c6247eddc154520d6674413
CesarHtea <chernandez_83@yahoo.com.mx>
2018-05-01 21:31:46 -0500

    Views

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const { Model } = require('objection')
+const ejs = require('ejs')
 const bodyParser = require('body-parser')
 
 const connectToDatabase = require('./src/database/connection')
 const knexFile = require('./knexfile')
 
+const pageRouter = require('./src/routes/pageRoutes')
 const apiRouter = require('./src/routes/apiRoutes')
                 
 const app = express()
@@ -13,14 +15,23 @@ const appConnectionWithDatabase = connectToDatabase(knexFile.development)
 
 Model.knex(appConnectionWithDatabase)
 
+// Setup EJS engine
+app.engine('ejs', ejs.renderFile)
+app.set('view engine', 'ejs')
+app.set('views', `${__dirname}/src/views`)
+
 // para las peticiones (e.g. POST)
 app.use(bodyParser.json())
 
-app.get('/', function(req, res){
-    res.send('home page')
-})
+// Setup static files
+app.use(express.static(`${__dirname}/public`))
 
+app.use('/', pageRouter)
 app.use('/api', apiRouter)
+// Show 404 view
+app.use(function(req, res) {
+  res.render('404.ejs')
+})
 
 const PORT = process.env.PORT || 3000;app.listen(PORT, function(){
     console.log(`App running in PORT: ${PORT}`)
